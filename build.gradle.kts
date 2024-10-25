79b7e9e5e1dd954f11aefa46d0af4976ecb852f4
nicolasrodf <nicolas.rod.f@gmail.com>
2024-10-25 15:01:14 -0500

    install git hook for detekt running before each commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,25 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+tasks.register("copyGitHooks", Copy::class.java) {
+    description = "Copies the git hooks from /git-hooks to the .git folder."
+    group = "git hooks"
+    from("$rootDir/scripts/pre-commit")
+    into("$rootDir/.git/hooks/")
+}
+tasks.register("installGitHooks", Exec::class.java) {
+    description = "Installs the pre-commit git hooks from /git-hooks."
+    group = "git hooks"
+    workingDir = rootDir
+    commandLine = listOf("chmod")
+    args("-R", "+x", ".git/hooks/")
+    dependsOn("copyGitHooks")
+    doLast {
+        logger.info("Git hook installed successfully.")
+    }
+}
+
+afterEvaluate {
+    tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
+}
--- home/home_presentation/src/main/java/com/nicolasrf/home_presentation/detail/components/DetailFrequencyDate.kt
+++ home/home_presentation/src/main/java/com/nicolasrf/home_presentation/detail/components/DetailFrequencyDate.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nicolasrf.core_presentation.HabitCheckbox
-import com.nicolasrf.home_presentation.utils.Constants.Companion.DAY_LETTERS_QUANTITY
+import com.nicolasrf.home_presentation.utils.Constants.DAY_LETTERS_QUANTITY
 import java.time.DayOfWeek
 
 @Composable
